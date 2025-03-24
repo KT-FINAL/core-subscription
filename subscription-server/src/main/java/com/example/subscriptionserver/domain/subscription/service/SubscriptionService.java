cd93a2a46587f596d1dfe698f8283b8a0b164cfb
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-24 16:34:54 +0900

    [FEAT] Subscription Service

--- subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/service/SubscriptionService.java
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/service/SubscriptionService.java
@@ -1,5 +1,13 @@
 package com.example.subscriptionserver.domain.subscription.service;
 
+import com.example.subscriptionserver.domain.subscription.dto.request.SaveSubscriptionRequest;
+import com.example.subscriptionserver.domain.subscription.dto.response.SubscriptionResponse;
+
+
+import java.util.List;
+
 public interface SubscriptionService {
-    // CRUD
+    List<SubscriptionResponse> getSubscriptionsByMemberId(Long memberId);
+
+    void registerSubscription(SaveSubscriptionRequest saveSubscriptionRequest);
 }
--- subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/service/SubscriptionServiceImpl.java
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/service/SubscriptionServiceImpl.java
@@ -1,13 +1,36 @@
 package com.example.subscriptionserver.domain.subscription.service;
 
+import com.example.subscriptionserver.domain.subscription.dto.request.SaveSubscriptionRequest;
+import com.example.subscriptionserver.domain.subscription.dto.response.SubscriptionResponse;
+import com.example.subscriptionserver.domain.subscription.entity.Subscription;
+import com.example.subscriptionserver.domain.subscription.mapper.SubscriptionMapper;
 import com.example.subscriptionserver.domain.subscription.repository.SubscriptionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscriptionServiceImpl implements SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
+    private final SubscriptionMapper subscriptionMapper;
 
-    public SubscriptionServiceImpl(SubscriptionRepository subscriptionRepository) {
+    public SubscriptionServiceImpl(
+            SubscriptionRepository subscriptionRepository,
+            SubscriptionMapper subscriptionMapper) {
         this.subscriptionRepository = subscriptionRepository;
+        this.subscriptionMapper = subscriptionMapper;
+    }
+
+    @Override
+    public List<SubscriptionResponse> getSubscriptionsByMemberId(Long memberId) {
+        return List.of();
+    }
+
+    @Override
+    public void registerSubscription(SaveSubscriptionRequest saveSubscriptionRequest) {
+        Subscription subscription = subscriptionRepository.save(
+                subscriptionMapper.saveRequestToSubscription(saveSubscriptionRequest)
+        );
+
     }
 }
