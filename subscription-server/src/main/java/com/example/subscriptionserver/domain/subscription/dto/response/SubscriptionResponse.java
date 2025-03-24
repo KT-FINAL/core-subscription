fe618b51edc8fd1743a2146fe3a20c912898755e
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-24 16:35:00 +0900

    [FEAT] Subscription DTO

--- subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/dto/response/SubscriptionResponse.java
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/dto/response/SubscriptionResponse.java
@@ -1,11 +1,21 @@
 package com.example.subscriptionserver.domain.subscription.dto.response;
 
+import com.example.subscriptionserver.domain.subscription.entity.SubscriptionStatus;
+import com.example.subscriptionserver.domain.subscription.entity.SubscriptionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SubscriptionResponse {
+    private Long id;
+    private Long memberId;
+    private SubscriptionType subscriptionType;
+    private SubscriptionStatus subscriptionStatus;
+    private LocalDate startSubscriptionDate;
+    private LocalDate endSubscriptionDate;
 }
