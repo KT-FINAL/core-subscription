7a97b2dfd5c5e0f2a29c22730e50934b78f9dc88
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-24 10:27:47 +0900

    [FEAT] Init Setting

---
+++ subscription-server/src/main/java/com/example/subscriptionserver/SubscriptionServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionServerApplication.class, args);
+	}
+
+}
---
+++ subscription-server/src/test/java/com/example/subscriptionserver/SubscriptionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
