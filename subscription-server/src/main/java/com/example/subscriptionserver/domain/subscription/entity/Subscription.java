c0b7d7c8b8730e4d02de5e14b291ca33b8c5729f
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-24 13:20:16 +0900

    [FEAT] Subscription Directory Init Setting

--- subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/entity/Subscription.java
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/entity/Subscription.java
@@ -4,12 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    private Long memberId;
 }
---
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/entity/SubscriptionType.java
@@ -0,0 +1,15 @@
+package com.example.subscriptionserver.domain.subscription.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubscriptionType {
+
+}
