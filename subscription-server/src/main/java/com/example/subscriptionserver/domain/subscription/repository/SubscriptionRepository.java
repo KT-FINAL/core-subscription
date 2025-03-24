a47262486457d3cebd6cb9e0a37c991923c22a19
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-24 16:35:13 +0900

    [FEAT] Subscription Repository Default Method

--- subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/repository/SubscriptionRepository.java
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/repository/SubscriptionRepository.java
@@ -4,7 +4,11 @@ import com.example.subscriptionserver.domain.subscription.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByMemberId(Long memberId);
 
 }
