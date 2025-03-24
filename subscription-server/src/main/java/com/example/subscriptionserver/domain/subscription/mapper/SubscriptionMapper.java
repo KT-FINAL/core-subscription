c5ed8d07c3c9c21e585582dc62a57c69fb712125
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-24 16:34:38 +0900

    [FEAT] Subscription Mapper Init

--- subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/mapper/SubscriptionMapper.java
+++ subscription-server/src/main/java/com/example/subscriptionserver/domain/subscription/mapper/SubscriptionMapper.java
@@ -1,7 +1,16 @@
 package com.example.subscriptionserver.domain.subscription.mapper;
 
+import com.example.subscriptionserver.domain.subscription.dto.request.SaveSubscriptionRequest;
+import com.example.subscriptionserver.domain.subscription.entity.Subscription;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SubscriptionMapper {
+    public Subscription saveRequestToSubscription(SaveSubscriptionRequest request) {
+        return new Subscription(
+                request.getMemberId(),
+                request.getSubscriptionType(),
+                request.getSubscriptionStartDate()
+        );
+    }
 }
