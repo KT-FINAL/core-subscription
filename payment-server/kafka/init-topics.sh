e85f56d1c366c3d6c5f6b250f960393f7ea740d7
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-03-26 23:48:59 +0900

    [FEAT] Docker Compose 오류 수정

--- payment-server/kafka/init-topics.sh
+++ payment-server/kafka/init-topics.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# 내부 컨테이너에서 접근할 때는 kafka:29092
+BOOTSTRAP_SERVER="kafka:29092"
+
 echo "[INIT] Waiting for Kafka to be fully ready..."
 
 RETRIES=30
-until kafka-topics --bootstrap-server kafka:9092 --list >/dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until /usr/bin/kafka-topics --bootstrap-server $BOOTSTRAP_SERVER --list >/dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for Kafka to be ready..."
   sleep 2
   ((RETRIES--))
@@ -16,13 +19,14 @@ fi
 
 echo "[INIT] Kafka is ready. Creating topics..."
 
-kafka-topics --create --if-not-exists --bootstrap-server kafka:9092 \
+# 토픽 생성
+/usr/bin/kafka-topics --create --if-not-exists --bootstrap-server $BOOTSTRAP_SERVER \
   --replication-factor 1 --partitions 3 --topic payment-completed
 
-kafka-topics --create --if-not-exists --bootstrap-server kafka:9092 \
+/usr/bin/kafka-topics --create --if-not-exists --bootstrap-server $BOOTSTRAP_SERVER \
   --replication-factor 1 --partitions 3 --topic subscription-failed
 
-kafka-topics --create --if-not-exists --bootstrap-server kafka:9092 \
+/usr/bin/kafka-topics --create --if-not-exists --bootstrap-server $BOOTSTRAP_SERVER \
   --replication-factor 1 --partitions 3 --topic member-notification-topic
 
 echo "[INIT] Topic creation complete."
